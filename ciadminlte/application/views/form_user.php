0915535aa98c1e4edc0a502de34f10b1d65c7d21
unknown <you@example.com>
2018-04-27 16:23:39 +0700

    first commit

---
+++ ciadminlte/application/views/form_user.php
@@ -0,0 +1,26 @@
+username,password,fullname ,level
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset= "utf-8"/>
+    <title>Form Input - User</title>
+</head>
+<body>
+    <form action = "" method ="post">
+        Username<br/>
+        <input type="text" name="username" /><br/>
+        Password<br/>
+        <input type="password" name="password" /><br/>
+        Fullname<br/>
+        <input type="text" name="fullname" /><br/>
+        Fullname<br/>
+        <input type="text" name="fullname" /><br/>
+        Level<br/>
+        <select name="level">
+            <option value = "admin" >Admin</option>
+            <option value = "user" > User</option>
+        </select>
+    </form>
+</body>
+</html>
